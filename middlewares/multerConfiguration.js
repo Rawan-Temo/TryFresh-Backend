455ca8e19e6efa0372f370380f2b52cf42ee3551
JOKER2Q <rinas1122k@gmail.com>
2025-10-14 13:03:53 +0300

    test

--- middlewares/multerConfiguration.js
+++ middlewares/multerConfiguration.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const path = require("path");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join("../public")); // Save files to the desired folder
+    cb(null, path.join(__dirname, "../public")); // Save files to the desired folder
   },
   filename: function (req, file, cb) {
     // Generate a unique file name with timestamp
