f801ef9f808412291857cd7dec7310236ce5ebde
JOKER2Q <rinas1122k@gmail.com>
2025-10-14 11:54:35 +0300

    bb

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ app.use(morgan("dev"));
 app.use("/api/users", require("./routes/userRouter"));
 app.use("/api/types", require("./routes/typeRouter"));
 app.use("/api/items", require("./routes/itemRouter"));
+
+// Serve React build files
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
+});
+// // API Routes Ends
+// // 404 Handler
+// app.use((req, res, next) => {
+//   res.status(404).json({ status: "fail", message: "Route not found" });
+// });
+
+// // Global Error Handling Middleware
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).json({ status: "error", message: "Something went wrong" });
+// });
+
 app.listen("8000", () => {
   console.log("server is running on port 8000");
 });
